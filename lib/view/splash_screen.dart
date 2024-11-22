29517668417ea030a191edf71b53070697080f9e
mahdi-khoshkhabar <mahdikhoshkhabar1382@gmail.com>
2024-11-22 17:37:37 +0330

    Fix: put back main screen view

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:tech_blog_v2/gen/assets.gen.dart';
 import 'package:tech_blog_v2/my_colors.dart';
 import 'package:tech_blog_v2/view/complete_information_screen.dart';
+import 'package:tech_blog_v2/view/main_screen.dart';
 // import 'package:tech_blog_v2/view/main_screen.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -18,16 +19,14 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(milliseconds: 1)).then(
-      // Future.delayed(const Duration(milliseconds: 2650)).then(
+      Future.delayed(const Duration(milliseconds: 2650)).then(
       (value) {
         // Navigator.of(context)
         //     .pushReplacement(MaterialPageRoute(builder: (context) => const MainScreen()));
 
         if (mounted) {
           Navigator.of(context).pushReplacement(
-              CupertinoPageRoute(builder: (context) => const CompleteInfoScreen()));
-              // CupertinoPageRoute(builder: (context) => const MainScreen()));
+              CupertinoPageRoute(builder: (context) => const MainScreen()));
         }
       },
     );
