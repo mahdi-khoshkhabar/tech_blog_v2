2aff4289cf776aab337cf121fd18e11cf408f114
mahdi-khoshkhabar <mahdikhoshkhabar1382@gmail.com>
2025-02-04 01:19:50 +0330

    remove unnecessary imports

--- lib/model/items.dart
+++ lib/model/items.dart
@@ -4,10 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tech_blog_v2/controller/size_controller.dart';
 import 'package:tech_blog_v2/gen/assets.gen.dart';
-import 'package:tech_blog_v2/model/article_model.dart';
-import 'package:tech_blog_v2/model/podcast_model.dart';
 import 'package:tech_blog_v2/utils/my_colors.dart';
-import 'package:tech_blog_v2/utils/my_string.dart';
 
 class ItemsBasicIngredients {
   poster(Size size, Size? itemPosterSize, Image? posterImage, String auther,
--- lib/view/body/home_screen.dart
+++ lib/view/body/home_screen.dart
@@ -3,7 +3,6 @@
 import 'package:flutter/material.dart';
 import 'package:tech_blog_v2/controller/home_screen_controller.dart';
 import 'package:tech_blog_v2/controller/size_controller.dart';
-import 'package:tech_blog_v2/gen/assets.gen.dart';
 import 'package:tech_blog_v2/model/article_model.dart';
 import 'package:tech_blog_v2/model/items.dart';
 import 'package:tech_blog_v2/model/main_screen_poster.dart';
